6b4ab1005139074d4d1805d5f550ca9afe0e2d9e
Klaris <128941859+KlarisGizdov@users.noreply.github.com>
2023-10-08 13:48:15 +0300

    Delete FIRST STEPS IN CODING/WEEK #6/nested loops directory

--- FIRST STEPS IN CODING/WEEK #6/nested loops/nested loops/multiplication-table.js
+++
@@ -1,8 +0,0 @@
-function multiTable(input) {
-    for(let x = 1; x <= 10; x++) {
-        for(let y = 1; y <= 10; y++) {
-            let answer = x * y;
-            console.log(`${x} * ${y} = ${answer}`);
-        }
-    }
-}
